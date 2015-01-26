0c247787fd2aa7258caa100018e1fe6ecf125235
Hao Lian <hi@haolian.org>
2015-01-26 14:26:31 -0500

    serialization

--- src/Main.hs
+++ src/Main.hs
@@ -2,31 +2,56 @@
 
 module Main where
 
-import           BasePrelude
-import qualified Control.Concurrent.MVar as M
+import           Control.Concurrent.Suspend.Lifted (sDelay)
 import           Control.Monad.Trans (liftIO)
-import           Data.Text.Encoding (decodeUtf8)
-import           Data.Time.Clock (getCurrentTime, utctDay, UTCTime)
 import           Data.Text (Text)
+import           Data.Text.Encoding (decodeUtf8)
+import           Data.Time.Calendar.OrdinalDate (sundayStartWeek)
+import           Data.Time.Clock (getCurrentTime, utctDay, UTCTime)
+import           Snap.Core hiding (path)
+import           Snap.Util.FileServe (serveDirectory)
+
+import qualified Control.Concurrent.MVar as M
+import qualified Control.Concurrent.Timer as D4
+import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
-import           Data.Time.Calendar.OrdinalDate
+
+import           BasePrelude
+import           Data.Aeson
 import           Lucid
-import           Snap.Core hiding (path)
 import           Snap.Http.Server.Config
-import           Snap.Util.FileServe
 import           Snap.Http.Server
 
-data Feeling = Feeling Text deriving (Show, Eq)
+data Feeling = Feeling UTCTime Text deriving (Show, Eq)
+
+instance FromJSON Feeling where
+  parseJSON (Object o) =
+    Feeling <$> o .: "time"
+            <*> o .: "text"
+  parseJSON _ = error "FromJSON Feeling: expecting object"
+
+instance ToJSON Feeling where
+  toJSON (Feeling time text) =
+    object ["time" .= time, "text" .= text]
 
 main :: IO ()
 main = do
-  feelings <- liftIO (M.newMVar [ Feeling "i like you"
-                                , Feeling "i don't like you"
-                                , Feeling "i like you more"])
+  feelings <- initialFeelingsM
+  D4.repeatedTimer (serialize feelings) (sDelay 1)
+
   httpServe config (site feelings)
   where
     config = setPort 4445 mempty
+
+initialFeelingsM :: IO (M.MVar [Feeling])
+initialFeelingsM =
+  join $ (M.newMVar . fromJust . decode) <$> BSL.readFile "feelings.txt"
+
+serialize :: MVar [Feeling] -> IO ()
+serialize feelingsM = do
+  feelings <- M.readMVar feelingsM
+  BSL.writeFile "feelings.txt" (encode feelings)
 
 site :: M.MVar [Feeling] -> Snap ()
 site feelingsM = do
@@ -42,15 +67,16 @@ present = T.pack . show
 dayOfWeek :: UTCTime -> Int
 dayOfWeek = snd . sundayStartWeek . utctDay
 
+isFriday :: UTCTime -> Bool
+isFriday = (== 5) . dayOfWeek
+
 feeling :: MVar [Feeling] -> Snap ()
 feeling feelingsM = do
   req <- getRequest
+  time <- liftIO getCurrentTime
   let text = (decodeUtf8 . head . fromJust) (rqPostParam "text" req)
-  liftIO $ M.modifyMVar_ feelingsM (htmlM text)
+  liftIO $ M.modifyMVar_ feelingsM (return . (:) (Feeling time text))
   redirect "/"
-  where
-    htmlM text feelings = do
-      return (Feeling text : feelings)
 
 home :: MVar [Feeling] -> Snap ()
 home feelingsM = do
@@ -58,8 +84,7 @@ home feelingsM = do
   toLucid <- liftIO $ M.modifyMVar feelingsM (htmlM day)
   lucid toLucid
   where
-    htmlM day feelings = do
-      putStrLn (show feelings)
+    htmlM day feelings =
       return (feelings, html' day feelings)
 
     html' :: Int -> [Feeling] -> Html ()
@@ -67,7 +92,7 @@ home feelingsM = do
       doctype_
       html_ (head' <> body' day feelings)
 
-    head' = do
+    head' =
       head_ (title_ "feelings.blackfriday" <>
              css "static/css/css.css.css" <>
              meta_ [charset_ "utf-8"])
@@ -82,7 +107,8 @@ home feelingsM = do
       forM_ feelings feeling'
 
     feeling' :: Feeling -> Html ()
-    feeling' (Feeling i) = p_ (toHtml i)
+    feeling' (Feeling t i) =
+      with p_ [class_ (if isFriday t then "friday" else "timeslip")] (toHtml i)
 
     good = do
       p_ "it's friday"
@@ -91,7 +117,7 @@ home feelingsM = do
                  ]
         (p_ (textarea_ [placeholder_ "type a feeling", name_ "text"] "") <>
          p_ (input_ [type_ "submit"]))
-    bad = do
+    bad =
       p_ "come back when it's friday"
     cheating =
       True
